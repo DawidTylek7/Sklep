d041b5ff4ffe26cadc660f19f81dc0fc965bf553
DawidTylek7 <dawid.tylek1@microsoft.wsei.edu.pl>
2022-07-06 21:45:16 +0200

    Dodanie produktów

---
+++ Shop.WPF/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop.WPF
+{
+    class Context : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=products.db"); 
+         //   optionsBuilder.UseLazyLoadingProxies();
+        }
+    }
+}

1bb4a2eff510f0e3be53f94b4fe263ab98035d67
DawidTylek7 <dawid.tylek1@microsoft.wsei.edu.pl>
2022-07-15 21:21:12 +0200

    Add validation for other textboxes

--- Shop.WPF/MainWindow.xaml.cs
+++ Shop.WPF/MainWindow.xaml.cs
@@ -227,7 +227,7 @@ namespace Shop.WPF
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
+            Regex regex = new Regex("[^0-9,]+");
             e.Handled = regex.IsMatch(e.Text);
         }
     }
